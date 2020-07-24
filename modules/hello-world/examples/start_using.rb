c1561f1378699cf63b8902418e2c37493fffb172
Richard Smedley <rgsmedley@gmail.com>
2020-07-24 13:50:14 +0100

    tags & #

--- modules/hello-world/examples/start_using.rb
+++ modules/hello-world/examples/start_using.rb
@@ -1,28 +1,28 @@
-# #tag::imports[]
+# tag::imports[]
 require "couchbase"
 include Couchbase # to avoid repeating module name
-# #end::imports[]
+# end::imports[]
 
-# #tag::connect[]
+# tag::connect[]
 options = Cluster::ClusterOptions.new
 options.authenticate("Administrator", "password")
 cluster = Cluster.connect("couchbase://localhost", options)
-# #end::connect[]
+# end::connect[]
 
-# #tag::bucket[]
+# tag::bucket[]
 # get a bucket reference
 bucket = cluster.bucket("bucket-name")
-# #end::bucket[]
+#end::bucket[]
 
-# #tag::collection[]
+# tag::collection[]
 # get a collection reference
 collection = bucket.default_collection
 
 # or for named collection
 named_collection = bucket.scope("myapp").collection("my-collection")
-# #end::collection[]
+# end::collection[]
 
-# #tag::upsert-get[]
+# tag::upsert-get[]
 # Upsert Document
 upsert_result = collection.upsert(
   "u:king_arthur",
@@ -43,16 +43,16 @@ get_result = collection.get("u:king_arthur")
 p cas: get_result.cas,
   name: get_result.content["name"]
 #=> {:cas=>223322674373654, :name=>"Arthur"}
-# #end::upsert-get[]
+# end::upsert-get[]
 
-# #tag::n1ql-query[]
+# tag::n1ql-query[]
 result = cluster.query('SELECT "Hello World" AS greeting')
 result.rows.each do |row|
   p row
   #=> {"greeting"=>"Hello World"}
 end
-# #end::n1ql-query[]
+# end::n1ql-query[]
 
-# #tag::disconnect[]
+# tag::disconnect[]
 cluster.disconnect
-# #end::disconnect[]
+# end::disconnect[]
